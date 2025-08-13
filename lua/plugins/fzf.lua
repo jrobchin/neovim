d45ede6f9f239218c8d48a3785f5ea940f19a06d
jrobchin <jason.rob.chin@gmail.com>
2025-08-13 06:06:43 -0700

    clean up

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,15 +1,16 @@
 return {
-  {
-    "ibhagwan/fzf-lua",
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    keys = {
-      { "<leader>f", "<cmd>FzfLua files<cr>",      desc = "Find files" },
-      { "<leader>F", "<cmd>FzfLua live_grep<cr>",  desc = "Live grep (search content)" },
-      { "<leader>b", "<cmd>FzfLua buffers<cr>",    desc = "List buffers" },
-      { "<leader>h", "<cmd>FzfLua help_tags<cr>",  desc = "Search help tags" },
-    },
-    config = function()
-      require("fzf-lua").setup()
-    end,
-  },
+	{
+		"ibhagwan/fzf-lua",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		keys = {
+			{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find files" },
+			{ "<leader>fr", "<cmd>FzfLua files resume=true<cr>", desc = "Resume finding files" },
+			{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live grep (search content)" },
+			{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "List buffers" },
+			{ "<leader>fh", "<cmd>FzfLua help_tags<cr>", desc = "Search help tags" },
+		},
+		config = function()
+			require("fzf-lua").setup()
+		end,
+	},
 }
---
+++ lua/plugins/misc.lua
@@ -0,0 +1,39 @@
+return {
+	{
+		"vague2k/vague.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vague").setup({})
+			vim.cmd("colorscheme vague")
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+	{ "folke/todo-comments.nvim", opts = {} },
+	{
+		"hedyhli/outline.nvim",
+		keys = {
+			{ "<leader>o", "<cmd>Outline<cr>", desc = "Toggle outline" },
+		},
+		config = function()
+			require("outline").setup({
+				providers = {
+					priority = { "lsp", "coc", "markdown", "norg", "treesitter" },
+				},
+			})
+		end,
+		event = "VeryLazy",
+		dependencies = {
+			"epheien/outline-treesitter-provider.nvim",
+		},
+	},
+	{
+		"tpope/vim-sleuth",
+	},
+}

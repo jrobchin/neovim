33554a95384ef90272a87401854568fdfe6dd92c
jrobchin <jason.rob.chin@gmail.com>
2025-08-13 07:08:36 -0700

    lsp start

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,54 +2,57 @@ local wk = require("which-key")
 
 -- Quickfix toggle
 local function toggle_quickfix()
-  local is_open = false
-  for _, win in ipairs(vim.fn.getwininfo()) do
-    if win.quickfix == 1 then
-      is_open = true
-      break
-    end
-  end
-  if is_open then
-    vim.cmd("cclose")
-  else
-    vim.cmd("copen")
-  end
+	local is_open = false
+	for _, win in ipairs(vim.fn.getwininfo()) do
+		if win.quickfix == 1 then
+			is_open = true
+			break
+		end
+	end
+	if is_open then
+		vim.cmd("cclose")
+	else
+		vim.cmd("copen")
+	end
 end
 
 -- Location list toggle (for current window)
 local function toggle_loclist()
-  local ll = vim.fn.getloclist(0, { winid = 0 })
-  if ll.winid ~= 0 then
-    vim.cmd("lclose")
-  else
-    vim.cmd("lopen")
-  end
+	local ll = vim.fn.getloclist(0, { winid = 0 })
+	if ll.winid ~= 0 then
+		vim.cmd("lclose")
+	else
+		vim.cmd("lopen")
+	end
 end
 
 wk.add({
-  {
-    mode = { "n", "x" },
-    { "j", "gj", desc = "Navigate down (visual line)" },
-    { "k", "gk", desc = "Navigate up (visual line)" },
-    { "<Down>", "gj", desc = "Navigate down (visual line)" },
-    { "<Up>",   "gk", desc = "Navigate up (visual line)" },
-    { "<C-l>",  ":set hlsearch!<cr><C-l>", desc = "Toggle search highlighting" },
+	{
+		mode = { "n", "x" },
+		{ "j", "gj", desc = "Navigate down (visual line)" },
+		{ "k", "gk", desc = "Navigate up (visual line)" },
+		{ "<Down>", "gj", desc = "Navigate down (visual line)" },
+		{ "<Up>", "gk", desc = "Navigate up (visual line)" },
+		{ "<C-l>", ":set hlsearch!<cr><C-l>", desc = "Toggle search highlighting" },
 
-    -- System clipboard
-    { "<leader>y", '"+y', desc = "Copy to system clipboard" },
-    { "<leader>p", '"+p', desc = "Paste from system clipboard after cursor" },
-    { "<leader>P", '"+P', desc = "Paste from system clipboard before cursor" },
+		-- Buffers
+		{ "<leader>gb", "<cmd>bnext<cr>", desc = "Next buffer" },
+		{ "<leader>gB", "<cmd>bprev<cr>", desc = "Previous buffer" },
 
-    -- Toggles
-    { "<leader>q", toggle_quickfix, desc = "Toggle quickfix list", mode = "n" },
-    { "<leader>l", toggle_loclist,  desc = "Toggle location list", mode = "n" },
-  },
+		-- System clipboard
+		{ "<leader>y", "\"+y", desc = "Copy to system clipboard" },
+		{ "<leader>p", "\"+p", desc = "Paste from system clipboard after cursor" },
+		{ "<leader>P", "\"+P", desc = "Paste from system clipboard before cursor" },
 
-  -- Insert
-  { "<Down>", "<C-\\><C-o>gj", desc = "Navigate down (visual line)", mode = "i" },
-  { "<Up>",   "<C-\\><C-o>gk", desc = "Navigate up (visual line)",   mode = "i" },
+		-- Toggles
+		{ "<leader>q", toggle_quickfix, desc = "Toggle quickfix list", mode = "n" },
+		{ "<leader>l", toggle_loclist, desc = "Toggle location list", mode = "n" },
+	},
 
-  -- Visual-only
-  { "<leader>x", "<Cmd>:lua<CR>", desc = "Lua: execute current selection", mode = "v" },
+	-- Insert
+	{ "<Down>", "<C-\\><C-o>gj", desc = "Navigate down (visual line)", mode = "i" },
+	{ "<Up>", "<C-\\><C-o>gk", desc = "Navigate up (visual line)", mode = "i" },
+
+	-- Visual-only
+	{ "<leader>x", "<Cmd>:lua<CR>", desc = "Lua: execute current selection", mode = "v" },
 })
-
--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,10 +4,14 @@ return {
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		keys = {
 			{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find files" },
-			{ "<leader>fr", "<cmd>FzfLua files resume=true<cr>", desc = "Resume finding files" },
+			{ "<leader>fF", "<cmd>FzfLua files resume=true<cr>", desc = "Resume finding files" },
 			{ "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live grep (search content)" },
-			{ "<leader>fb", "<cmd>FzfLua buffers<cr>", desc = "List buffers" },
-			{ "<leader>fh", "<cmd>FzfLua help_tags<cr>", desc = "Search help tags" },
+			{ "<leader>fG", "<cmd>FzfLua live_grep resume=true<cr>", desc = "Live grep (search content)" },
+			{ "<leader>fh", "<cmd>FzfLua help_tags<cr>", desc = "Find help tags" },
+			{ "<leader>fi", "<cmd>FzfLua builtin<cr>", desc = "Find fzf buildins" },
+			{ "<leader>fk", "<cmd>FzfLua keymaps<cr>", desc = "Find keymaps" },
+			{ "<leader><leader>", "<cmd>FzfLua buffers<cr>", desc = "List buffers" },
+			{ "<leader>/", "<cmd>FzfLua lgrep_curbuf<cr>", desc = "Find fzf buildins" },
 		},
 		config = function()
 			require("fzf-lua").setup()
